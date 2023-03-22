fc7efdbd4b62b3606c1cbcda57bc349ca43b9019
haoling <maya@fei-yen.jp>
2023-03-22 14:43:02 +0900

    commit

--- app.py
+++ app.py
@@ -2,87 +2,126 @@
 # [LangChain の Googleカスタム検索 連携を試す｜npaka｜note](https://note.com/npaka/n/nd9a4a26a8932)
 # [LangChainを使ってOSINTを自動化する](https://zenn.dev/tatsui/articles/c4b4f796a85395)
 
+# https://github.com/hwchase17/langchain/blob/master/langchain/agents/conversational_chat/base.py
+# https://github.com/hwchase17/langchain/blob/master/langchain/agents/conversational_chat/prompt.py
+
 import gradio as gr
 import os, json, sys, re
-from langchain import LLMChain, ConversationChain
+from typing import Any, List, Optional, Sequence, Tuple
+from langchain import LLMChain
 from langchain.chat_models import ChatOpenAI
-from langchain.agents import ConversationalAgent, Tool, AgentExecutor
+from langchain.agents import Tool, AgentExecutor
+from langchain.agents.conversational_chat.base import ConversationalChatAgent, AgentOutputParser
 from langchain.utilities import GoogleSearchAPIWrapper
 from langchain.chains.conversation.memory import ConversationBufferMemory
 from langchain.callbacks import get_openai_callback
+from langchain.schema import (
+    AIMessage,
+    BaseMessage,
+    ChatMessage,
+    HumanMessage,
+    PromptValue,
+    SystemMessage,
+)
+
+llm_gpt = ChatOpenAI(
+    temperature=0.95,
+    model_name="gpt-3.5-turbo",
+)
+search = GoogleSearchAPIWrapper()
+search_news = GoogleSearchAPIWrapper(google_cse_id=os.environ["GOOGLE_CSE_ID_NEWS"])
+tools = [
+    Tool(
+        name = "News search",
+        func=search_news.run,
+        description="読売新聞のサイトを検索することができます。最新の話題について答える場合に優先的に利用することができます。入力は検索内容です。検索内容は日本語で入力します。"
+    ),
+    Tool(
+        name = "Google Search",
+        func=search.run,
+        description="知らないことを調べるときに利用することができます。また、今日の日付や為替レートなど現在の状況についても確認することができます。また、アニメや漫画の質問に答える必要がある場合に役立ちます。入力は検索内容です。検索内容は日本語で入力します。"
+    ),
+]
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+
+PREFIX = """アシスタントは、OpenAIによって訓練された大型言語モデルです。
+アシスタントは、単純な質問に答えるだけでなく、幅広いトピックについて詳しい説明や議論を提供することができるよう設計されています。言語モデルとして、アシスタントは受け取った入力に基づいて人間らしいテキストを生成することができ、自然な会話を行い、話題に関連する意味のある回答を提供することができます。
+アシスタントは常に学習して改善しており、その能力は常に進化しています。多くのテキストを処理し理解することができ、これらの知識を利用して幅広い質問に対して正確で情報量の多い回答を提供することができます。さらに、アシスタントは、受け取った入力に基づいて自分自身のテキストを生成することができ、幅広いトピックに関して説明や説明を行い、議論を展開することができます。
+全体として、アシスタントは幅広いタスクに対応することができ、幅広いトピックについて貴重な情報を提供することができる強力なシステムです。特定の質問についてのヘルプが必要な場合や、特定のトピックについての会話をしたい場合には、アシスタントがお手伝いします。"""
+
+FORMAT_INSTRUCTIONS = """RESPONSE FORMAT INSTRUCTIONS
+----------------------------
+When responding to me please, please output a response in one of two formats:
+**Option 1:**
+Use this if you want the human to use a tool.
+Markdown code snippet formatted in the following schema:
+```json
+{{{{
+    "action": string \\ The action to take. Must be one of {tool_names}
+    "action_input": string \\ The input to the action
+}}}}
+```
+**Option #2:**
+Use this if you want to respond directly to the human. Markdown code snippet formatted in the following schema:
+```json
+{{{{
+    "action": "Final Answer",
+    "action_input": string \\ You should put what you want to return to use here. Plaese follow character role guideline.
+}}}}
+```"""
+
+SUFFIX_TEMPLATE = """TOOLS
+------
+Assistant can ask the user to use tools to look up information that may be helpful in answering the users original question. The tools the human can use are:
+
+{{tools}}
+
+{format_instructions}
+
+CHAT HISTORY
+--------------------
+Here is the chat history.
+%s
+
+USER'S INPUT
+--------------------
+Here is the user's input (remember to respond with a markdown code snippet of a json blob with a single action, and NOTHING else):
+{{{{input}}}}"""
+
+class MyAgentOutputParser(AgentOutputParser):
+    def get_format_instructions(self) -> str:
+        return FORMAT_INSTRUCTIONS
+
+    def parse(self, text: str) -> Any:
+        if '"action"' in text and '"action_input"' in text:
+            text = re.sub('\n', '', text)
+            text = '{' + text.split("{", 1)[1]
+            print(text)
+        return super().parse(text)
 
 def chat(prefix_messages, user_input):
-    llm_gpt = ChatOpenAI(
-        temperature=0.95,
-        model_name="gpt-3.5-turbo",
-    )
-    search = GoogleSearchAPIWrapper()
-    search_news = GoogleSearchAPIWrapper(google_cse_id=os.environ["GOOGLE_CSE_ID_NEWS"])
-    tools = [
-        Tool(
-            name = "News search",
-            func=search_news.run,
-            description="最新の話題について答える場合に利用することができます。入力は検索内容です。検索内容は日本語で入力します。"
-        ),
-        Tool(
-            name = "Intermediate Answer",
-            func=search.run,
-            description="知らないことを調べるときに利用することができます。また、今日の日付や為替レートなど現在の状況についても確認することができます。また、アニメや漫画の質問に答える必要がある場合に役立ちます。入力は検索内容です。検索内容は日本語で入力します。"
-        ),
-    ]
-    memory = ConversationBufferMemory(memory_key="chat_history")
-    
-    system_message = []
+    prompt = ConversationalChatAgent.create_prompt(tools=tools, system_message=PREFIX)
+
+    memory.clear()
     input = ""
     output = ""
+    system_messages = []
     for message in json.loads(prefix_messages):
         if message['role'] == 'system':
-            system_message.append(message['content'])
+            system_messages.append(SystemMessage(content=message['content']))
         if message['role'] == 'assistant':
             output = message['content']
             memory.save_context({"input": input}, {"ouput": output})
         if message['role'] == 'user':
             input = message['content']
+    prompt.messages[0:0] = system_messages
+    print(memory)
 
-    prompt = ConversationalAgent.create_prompt(
-        tools=tools,
-        prefix="""AssistantはOpenAIによって訓練された大規模な言語モデルです。
-Assistantは、簡単な質問に答えるだけでなく、幅広いトピックについて詳細な説明や議論を行うことができるように設計されています。言語モデルとして、Assistantは入力に基づいて人間らしいテキストを生成できるため、自然な会話をすることができ、話題に沿った意味のある回答を提供することができます。
-Assistantは常に学習して改善し、その能力は常に進化しています。Assistantは大量のテキストを処理し理解することができ、この知識を活用して、幅広い質問に正確かつ有益な回答を提供できます。また、Assistantは受け取った入力に基づいて独自のテキストを生成することができ、幅広いトピックについて説明や説明を行い、議論に参加することができます。
-全体的に言えば、Assistantは幅広いタスクに役立ち、多くのトピックに対して貴重な洞察や情報を提供することができる強力なツールです。特定の質問に対する支援が必要な場合や、特定のトピックについての会話をしたい場合、Assistantがサポートしてくれます。
-
-TOOLS:
-------
-
-Assistantは下記のツールにアクセスできます:""",
-        format_instructions="""ツールを使うには、下記のフォーマットに従ってください:
-```
-Thought: Do I need to use a tool? Yes
-Action: the action to take, should be one of [{tool_names}]
-Action Input: アクションへの入力です。
-Observation: Actionの結果です。
-```
-Humanに結果を返信する必要があるとき、またはツールを使う必要が無いときに、必ず下記のフォーマットに従う必要があります:
-```
-Thought: Do I need to use a tool? No
-{ai_prefix}: [your response here]
-```""",
-        suffix="""When you have a response to say to the Human, you MUST role-play the character according to the Character role guidelines:
-%s
-
-Begin!
-
-Previous conversation history:
-{chat_history}
-
-New input: {input}
-{agent_scratchpad}""" % ("\n".join(system_message)),
-        input_variables=["input", "chat_history", "agent_scratchpad"],
-    )
-    print(prompt)
     llm_chain = LLMChain(llm=llm_gpt, prompt=prompt)
-    agent = ConversationalAgent(llm_chain=llm_chain, allowed_tools=[tool.name for tool in tools])
+    print(llm_chain)
+    agent = ConversationalChatAgent(llm_chain=llm_chain, allowed_tools=[tool.name for tool in tools], output_parser=MyAgentOutputParser())
     agent_executor = AgentExecutor.from_agent_and_tools(agent=agent, tools=tools, verbose=True, memory=memory)
+
     with get_openai_callback() as cb:
         for i in range(3):
             try:
@@ -95,7 +134,7 @@ New input: {input}
                 err = "{0}".format(e)
                 if err.startswith("Could not parse LLM output: "):
                     print("total_tokens: {0}".format(cb.total_tokens))
-                    return "Warning", re.sub('^Could not parse LLM output: `(.*)`$', r'\1', err), cb.total_tokens
+                    return "Warning", re.sub('^Could not parse LLM output: (.*)$', r'\1', err), cb.total_tokens
                 else:
                     continue
             except Exception as e:
